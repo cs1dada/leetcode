3f68a9817c0b633c700d9e0334df95237819ba97
cs1dada <q2275357@hotmail.com>
2016-10-21 11:43:29 +0800

    add question description

--- 114. Flatten Binary Tree to Linked List/114.c
+++ 114. Flatten Binary Tree to Linked List/114.c
@@ -1,3 +1,30 @@
+/*
+Given a binary tree, flatten it to a linked list in-place.
+
+For example,
+Given
+
+         1
+        / \
+       2   5
+      / \   \
+     3   4   6
+The flattened tree should look like:
+   1
+    \
+     2
+      \
+       3
+        \
+         4
+          \
+           5
+            \
+             6
+
+*/
+
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
--- 142. Linked List Cycle II/142.c
+++ 142. Linked List Cycle II/142.c
@@ -1,3 +1,12 @@
+/*
+Given a linked list, return the node where the cycle begins. 
+If there is no cycle, return null.
+
+Note: Do not modify the linked list.
+
+*/
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
